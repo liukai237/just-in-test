bab86c5bb4beb4830f9189f64db46fce48842318
Kai <kazzz@126.com>
2015-11-08 00:05:40 +0800

    first commit

---
+++ src/main/java/com/magalan/Application.java
@@ -0,0 +1,14 @@
+package com.magalan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Kai on 11/7/15.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
